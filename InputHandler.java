351ca48c7ea901a400d0eb723b60a649572ca964
nathan <nathanbanstess@gmail.com>
2016-01-31 17:30:23 +1100

    handler input going along with added featured from Menu.java, character validator for yes/no options in menu class

--- InputHandler.java
+++ InputHandler.java
@@ -11,7 +11,7 @@ public class InputHandler {
         scanner = new Scanner(System.in);
     }
 
-    public int getMenuInt() {
+    public int getMenuInt(int max) {
         boolean valid = false;
         int value = 0;
         while (!valid) {
@@ -19,7 +19,7 @@ public class InputHandler {
                 if (scanner.hasNextInt()) {
                     value = scanner.nextInt();
                     valid = true;
-                    if (value < 1 || value > 5) {
+                    if (value < 1 || value > max) {
                         valid = false;
                         throw new Exception("ERROR: Please enter one of the menu options");
                     }
@@ -52,7 +52,7 @@ public class InputHandler {
                         }
                     }
                     else {
-                        if (value < 1 || value > 4) {
+                        if (value < 1 || value > 15) {
                             valid = false;
                             throw new Exception("ERROR: Please enter dimension between 1 and 4");
                         }
@@ -91,6 +91,70 @@ public class InputHandler {
         }
 
         return value;
+    }
+
+    public int getColumns(int dimension) {
+        boolean valid = false;
+        int value = 0;
+        double temp;
+        while (!valid) {
+            try {
+                if (scanner.hasNextInt()) {
+                    value = scanner.nextInt();
+                }
+                else {
+                    throw new Exception("ERROR: Please enter a whole number");
+                }
+                temp = value;
+                for (int i = 0; i < dimension; i++) {
+                    temp /= 2;
+                    if (temp == 1.0) {
+                        break;
+                    }
+                }
+                if (((int)temp != 1) || (value > (int)Math.pow(2, dimension))) {
+                    throw new Exception("ERROR: Please enter a valid column size");
+                }
+                valid = true;
+            }
+            catch (Exception error) {
+                System.out.println(error.getMessage());
+                resetScanner();
+            }
+        }
+
+        return value;
+    }
+
+    public char getYesNo() {
+        boolean valid = false;
+        String value = "";
+        char returnValue;
+        while(!valid) {
+            try {
+                if(scanner.hasNext()) {
+                    value = scanner.next();
+                }
+                else {
+                    throw new Exception("ERROR: Please enter a character");
+                }
+
+                if(value.equals("y") || value.equals("n")) {
+                    valid = true;
+                }
+                else {
+                    throw new Exception("ERROR: Please enter y or n");
+                }
+            }
+            catch(Exception error) {
+                System.out.println(error.getMessage());
+                resetScanner();
+            }
+        }
+
+        returnValue = value.charAt(0);
+
+        return returnValue;
     }
 
     public void resetScanner() {
