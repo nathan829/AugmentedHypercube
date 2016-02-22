3bcb462fa392391fa2558eb5a2e9b39de8c52e7d
nathan <nathanbanstess@gmail.com>
2016-02-22 19:17:46 +1100

    included commented out methods which can replace the existing method if information needed on specific edge subsets of complement or hypercube edges

--- Binary.java
+++ Binary.java
@@ -64,32 +64,144 @@ public class Binary {
         return returnString;
     }
 
-    public boolean hypercubeEdgeTest(String str1, String str2) {
-        char[] arr1 = str1.toCharArray();
-        char[] arr2 = str2.toCharArray();
+    // checking for all types of hypercube edges
+   public boolean hypercubeEdgeTest(String str1, String str2) {
+       char[] arr1 = str1.toCharArray();
+       char[] arr2 = str2.toCharArray();
 
-        // will count the number of difference in bits between str1 and str2
-        int numberDifferences = 0;
+       // will count the number of difference in bits between str1 and str2
+       int numberDifferences = 0;
 
-        // obvious tests for non-equal strings
-        if(str1.length() != str2.length() || str1.isEmpty() || str2.isEmpty()) {
-            return false;
-        }
-        // Loop through the entire array to check if there are any differences
-        // will return false if we find more than one difference
-        for (int i = 0; i < str1.length(); i++) {
-            if (arr1[i] != arr2[i]) {
-                numberDifferences++;
-                if (numberDifferences > 1) {
-                    return false;
-                }
-            }
-        }
-        // Return false if the numberDifferences is 0 (meaning there are not any changes,
-        // hence the 2 strings do not differ by 1)
-        return !(numberDifferences == 0);
-    }
+       // obvious tests for non-equal strings
+       if(str1.length() != str2.length() || str1.isEmpty() || str2.isEmpty()) {
+           return false;
+       }
+       // Loop through the entire array to check if there are any differences
+       // will return false if we find more than one difference
+       for (int i = 0; i < str1.length(); i++) {
+           if (arr1[i] != arr2[i]) {
+               numberDifferences++;
+               if (numberDifferences > 1) {
+                   return false;
+               }
+           }
+       }
+       // Return false if the numberDifferences is 0 (meaning there are not any changes,
+       // hence the 2 strings do not differ by 1)
+       return !(numberDifferences == 0);
+   }
 
+
+    // public boolean hypercubeEdgeTest(String str1, String str2) {
+    //     char[] arr1 = str1.toCharArray();
+    //     char[] arr2 = str2.toCharArray();
+
+    //     // will count the number of difference in bits between str1 and str2
+    //     int numberDifferences = 0;
+
+    //     // obvious tests for non-equal strings
+    //     if(str1.length() != str2.length() || str1.isEmpty() || str2.isEmpty()) {
+    //         return false;
+    //     }
+
+    //     // complement in only the second bit
+    //     int bit = 5;
+
+    //     if(arr1[bit - 1] == arr2[bit - 1]) {
+    //         return false;
+    //     }
+    //     // Loop through the entire array to check if there are any differences
+    //     // will return false if we find more than one difference
+    //     for (int i = 0; i < str1.length(); i++) {
+    //         if (arr1[i] != arr2[i]) {
+    //             numberDifferences++;
+    //             if (numberDifferences > 1) {
+    //                 return false;
+    //             }
+    //         }
+    //     }
+    //     // Return false if the numberDifferences is 0 (meaning there are not any changes,
+    //     // hence the 2 strings do not differ by 1)
+    //     return !(numberDifferences == 0);
+    // }
+
+    // checking for all possible complement edges
+   // public boolean complementEdgeTest(String str1, String str2) {
+   //     char[] arr1 = str1.toCharArray();
+   //     char[] arr2 = str2.toCharArray();
+
+   //     boolean complement = false;
+
+   //     // obvious test for non-equal strings
+   //     if(str1.length() != str2.length() || str1.isEmpty() || str2.isEmpty()) {
+   //         return false;
+   //     }
+
+   //     // from left to right, when the bits become complement they must stay complement til the end
+   //     // of the string
+   //     for (int i = 0; i < str1.length(); i++) {
+   //         // Checking that the strings are the same (!complement)
+   //         if (!complement) {
+   //             if (arr1[i] != arr2[i]) {
+   //                 complement = true;
+   //                 if ((i == str1.length() - 1) && (arr1[i-1] == arr2[i-1])) {
+   //                     return false;
+   //                 }
+   //             }
+   //         }
+   //         else {
+   //             if (arr1[i] == arr2[i]) {
+   //                 return false;
+   //             }
+   //         }
+   //     }
+
+   //     return true;
+   // }
+
+    // making it only equal in the first bit (i = 2)
+   // public boolean complementEdgeTest(String str1, String str2) {
+   //     char[] arr1 = str1.toCharArray();
+   //     char[] arr2 = str2.toCharArray();
+
+   //     boolean complement = false;
+
+   //     // obvious test for non-equal strings
+   //     if(str1.length() != str2.length() || str1.isEmpty() || str2.isEmpty()) {
+   //         return false;
+   //     }
+
+   //     // from left to right, when the bits become complement they must stay complement til the end
+   //     // of the string
+   //     for (int i = 0; i < str1.length(); i++) {
+   //         // Checking that the strings are the same (!complement)
+   //         if (!complement) {
+   //             if (arr1[i] != arr2[i]) {
+   //                 complement = true;
+   //                 // to avoid covering same case tested in hypercube edges
+   //                 if ((i == str1.length() - 1) && (arr1[i-1] == arr2[i-1])) {
+   //                     return false;
+   //                 }
+   //             }
+   //             else if (i == 2) {
+   //                 return false;
+   //             }
+   //         }
+   //         else {
+   //             if (arr1[i] == arr2[i]) {
+   //                 return false;
+   //             }
+   //             else if (i == 1) {
+   //                 return false;
+   //             }
+
+   //         }
+   //     }
+
+   //     return true;
+   // }
+
+    // equal in the first 2 bit positions only (i = 3)
     public boolean complementEdgeTest(String str1, String str2) {
         char[] arr1 = str1.toCharArray();
         char[] arr2 = str2.toCharArray();
@@ -108,13 +220,20 @@ public class Binary {
             if (!complement) {
                 if (arr1[i] != arr2[i]) {
                     complement = true;
+                    // to avoid covering same case tested in hypercube edges
                     if ((i == str1.length() - 1) && (arr1[i-1] == arr2[i-1])) {
                         return false;
                     }
                 }
+                else if (i == 3) {
+                    return false;
+                }
             }
             else {
                 if (arr1[i] == arr2[i]) {
+                    return false;
+                }
+                else if ((i == 1) || (i == 2)) {
                     return false;
                 }
             }
@@ -122,6 +241,167 @@ public class Binary {
 
         return true;
     }
+
+    // equal in the first 3 bits only (i = 4)
+   // public boolean complementEdgeTest(String str1, String str2) {
+   //     char[] arr1 = str1.toCharArray();
+   //     char[] arr2 = str2.toCharArray();
+
+   //     boolean complement = false;
+
+   //     // obvious test for non-equal strings
+   //     if(str1.length() != str2.length() || str1.isEmpty() || str2.isEmpty()) {
+   //         return false;
+   //     }
+
+   //     // from left to right, when the bits become complement they must stay complement til the end
+   //     // of the string
+   //     for (int i = 0; i < str1.length(); i++) {
+   //         // Checking that the strings are the same (!complement)
+   //         if (!complement) {
+   //             if (arr1[i] != arr2[i]) {
+   //                 complement = true;
+   //                 // to avoid covering same case tested in hypercube edges
+   //                 if ((i == str1.length() - 1) && (arr1[i-1] == arr2[i-1])) {
+   //                     return false;
+   //                 }
+   //             }
+   //             else if (i == 4) {
+   //                 return false;
+   //             }
+   //         }
+   //         else {
+   //             if (arr1[i] == arr2[i]) {
+   //                 return false;
+   //             }
+   //             else if ((i == 1) || (i == 2) || (i == 3)) {
+   //                 return false;
+   //             }
+   //         }
+   //     }
+
+   //     return true;
+   // }
+
+    // equal only in the first 4 bit positions (i = 5)
+   // public boolean complementEdgeTest(String str1, String str2) {
+   //     char[] arr1 = str1.toCharArray();
+   //     char[] arr2 = str2.toCharArray();
+
+   //     boolean complement = false;
+
+   //     // obvious test for non-equal strings
+   //     if(str1.length() != str2.length() || str1.isEmpty() || str2.isEmpty()) {
+   //         return false;
+   //     }
+
+   //     // from left to right, when the bits become complement they must stay complement til the end
+   //     // of the string
+   //     for (int i = 0; i < str1.length(); i++) {
+   //         // Checking that the strings are the same (!complement)
+   //         if (!complement) {
+   //             if (arr1[i] != arr2[i]) {
+   //                 complement = true;
+   //                 // to avoid covering same case tested in hypercube edges
+   //                 if ((i == str1.length() - 1) && (arr1[i-1] == arr2[i-1])) {
+   //                     return false;
+   //                 }
+   //             }
+   //             else if (i == 5) {
+   //                 return false;
+   //             }
+   //         }
+   //         else {
+   //             if (arr1[i] == arr2[i]) {
+   //                 return false;
+   //             }
+   //             else if ((i == 1) || (i == 2) || (i == 3) || (i == 4)) {
+   //                 return false;
+   //             }
+   //         }
+   //     }
+
+   //     return true;
+   // }
+
+    // equal in first 5 bits (i = 6)
+   // public boolean complementEdgeTest(String str1, String str2) {
+   //     char[] arr1 = str1.toCharArray();
+   //     char[] arr2 = str2.toCharArray();
+
+   //     boolean complement = false;
+
+   //     // obvious test for non-equal strings
+   //     if(str1.length() != str2.length() || str1.isEmpty() || str2.isEmpty()) {
+   //         return false;
+   //     }
+
+   //     // from left to right, when the bits become complement they must stay complement til the end
+   //     // of the string
+   //     for (int i = 0; i < str1.length(); i++) {
+   //         // Checking that the strings are the same (!complement)
+   //         if (!complement) {
+   //             if (arr1[i] != arr2[i]) {
+   //                 complement = true;
+   //                 // to avoid covering same case tested in hypercube edges
+   //                 if ((i == str1.length() - 1) && (arr1[i-1] == arr2[i-1])) {
+   //                     return false;
+   //                 }
+   //             }
+   //             else if (i == 6) {
+   //                 return false;
+   //             }
+   //         }
+   //         else {
+   //             if (arr1[i] == arr2[i]) {
+   //                 return false;
+   //             }
+   //             else if ((i == 1) || (i == 2) || (i == 3) || (i == 4) || (i == 5)) {
+   //                 return false;
+   //             }
+   //         }
+   //     }
+
+   //     return true;
+   // }
+
+    // complement in all positions (i = 1)
+   // public boolean complementEdgeTest(String str1, String str2) {
+   //     char[] arr1 = str1.toCharArray();
+   //     char[] arr2 = str2.toCharArray();
+
+   //     boolean complement = false;
+
+   //     // obvious test for non-equal strings
+   //     if(str1.length() != str2.length() || str1.isEmpty() || str2.isEmpty()) {
+   //         return false;
+   //     }
+
+   //     // from left to right, when the bits become complement they must stay complement til the end
+   //     // of the string
+   //     for (int i = 0; i < str1.length(); i++) {
+   //         // Checking that the strings are the same (!complement)
+   //         if (!complement) {
+   //             if (arr1[i] != arr2[i]) {
+   //                 complement = true;
+   //                 // to avoid covering same case tested in hypercube edges
+   //                 if ((i == str1.length() - 1) && (arr1[i-1] == arr2[i-1])) {
+   //                     return false;
+   //                 }
+   //             }
+   //             if (i != 0) {
+   //                 return false;
+   //             }
+   //         }
+   //         else {
+   //             if (arr1[i] == arr2[i]) {
+   //                 return false;
+   //             }
+   //         }
+   //     }
+
+   //     return true;
+   // }
 
     public int binaryStringToInt(String binaryString) {
         char[] bits = binaryString.toCharArray();
@@ -136,5 +416,21 @@ public class Binary {
         }
 
         return returnNumber;
+    }
+
+    public String intToBinaryString(int num) {
+        String value = "";
+        String previous = "";
+        // initialising the binary string corresponding to integer value 0
+        for (int i = 0; i < dimension; i++) {
+            previous += "0";
+        }
+
+        for (int i = 0; i < num; i++) {
+            value = getNext(previous);
+            previous = value;
+        }
+
+        return value;
     }
 }
